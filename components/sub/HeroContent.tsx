9410a670a66f1d00515f5e3521ead28457f9c906
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-06-08 03:09:35 +0530

    chore: Update dependencies and fix navbar text

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -18,37 +18,43 @@ const HeroContent = () => {
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-        <motion.div
-          variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
-        >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
-          </h1>
-        </motion.div>
-
-        <motion.div
-          variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
-        >
-          <span>
-            Providing
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              {" "}
-              the best{" "}
-            </span>
-            project exprience
-          </span>
-        </motion.div>
-
-        <motion.p
-          variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px]"
-        >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
-        </motion.p>
+<motion.div
+  variants={slideInFromTop}
+  className="Welcome-box py-[10px] px-[9px] opacity-[0]"
+>
+  <img 
+    src="/imgi.jpeg" 
+    alt="description_of_image" 
+    className="w-48 h-48 rounded-full mr-3 filter saturate-200" 
+  />
+  <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+</motion.div>
+<motion.div
+  variants={slideInFromLeft(0.5)}
+  className="flex flex-col gap-6 mt-6 text-3xl font-bold text-gray-300 max-w-[600px] w-auto h-auto"
+>
+  <span className="overflow-hidden border-r border-transparent inline-block">
+    <span className="animate-type">
+      My Self
+      <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+        {" "}
+        Soumyaranjan
+      </span>
+    </span>
+  </span>
+  <span>
+    A Frontend Developer.
+  </span>
+</motion.div>
+<motion.p
+  variants={slideInFromLeft(0.8)}
+  className="text-lg text-gray-400 my-5 max-w-[600px] text-justify"
+>
+  I'm a dedicated Frontend Web Developer with a passion for creating intuitive, user-friendly, and high-performance websites. I have a strong understanding of modern web technologies and a keen eye for design.
+  <br />
+  <br />
+  Smart India Hackathon Winner 2023 🏆 | 👨🏻‍🎓Student | Future Full Stack Dev 🌐 | Tech Enthusiast 💻.
+</motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
