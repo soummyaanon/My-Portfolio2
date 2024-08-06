ade4ff794692a139fd235935898f460e3261ab3d
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-08-06 19:56:53 +0530

    ```text chore: Update visitor count and next.config.js
    This commit updates the visitor count in the visitorCount.json file from 14 to 18. It also modifies the next.config.js file by removing the experimental section and adding comments to indicate that it can be removed or updated if needed. These changes were made to ensure accurate visitor count tracking and improve the configuration file.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  // Remove or update the experimental section if needed
+  // experimental: {
+  //   appDir: true,
+  // },
+}
+
+module.exports = nextConfig
